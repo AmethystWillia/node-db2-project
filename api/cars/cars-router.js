ac02b3dc07f520bce93bd79dd0ecf8de63f30baa
Amethyst Willia <zmithic@gmail.com>
2022-02-15 15:37:17 -0600

    Implemented everything needed for getAll

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,14 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const Cars = require('./cars-model');
+
+router.get('/', (req, res, next) => {
+    Cars.getAll()
+        .then(all => {
+            res.status(200).json(all);
+        })
+        .catch(err => {
+            next(err);
+        });
+});
+
+module.exports = router;
