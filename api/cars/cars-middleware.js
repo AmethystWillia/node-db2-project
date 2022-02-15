4139bbbe487f1a533df458d1996367c45900d9f7
Amethyst Willia <zmithic@gmail.com>
2022-02-15 15:58:20 -0600

    Implemented getById

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,20 @@
+const Cars = require('./cars-model');
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+
+    Cars.getById(id)
+        .then(result => {
+            if (result === null || result === undefined) {
+                res.status(404).json({ message: `car with id ${id} is not found` });
+            } else {
+                req.car = result;
+                next();
+            }
+        })
+        .catch(err => {
+            next(err);
+        });
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -13,3 +28,7 @@ const checkVinNumberValid = (req, res, next) => {
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
 }
+
+module.exports = {
+  checkCarId,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Cars = require('./cars-model');
+const { checkCarId } = require('./cars-middleware');
 
 router.get('/', (req, res, next) => {
     Cars.getAll()
@@ -9,6 +10,10 @@ router.get('/', (req, res, next) => {
         .catch(err => {
             next(err);
         });
+});
+
+router.get('/:id', checkCarId, (req, res) => {
+    res.status(200).json(req.car);
 });
 
 module.exports = router;
